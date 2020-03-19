23f0a0871606e5c52eba561b2628f9efda77aade
HP <HP@10.0.0.12>
2020-03-19 18:59:50 +0200

    3rd commit

--- CNCserverPro/src/main/java/controllers/KeyController.java
+++ CNCserverPro/src/main/java/controllers/KeyController.java
@@ -1,13 +1,11 @@
 package controllers;
 
 import java.util.Optional;
-
 import javax.crypto.spec.SecretKeySpec;
 import javax.inject.Inject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import Payment.CreditCard;
 import Payment.PaymentProcess;
 import entities.KeyEntity;
@@ -21,17 +19,17 @@ public class KeyController {
 	private KeyRepository repository;
 
 
-	public void gotRequest(@RequestParam String ip) {
+	/*public void requestForKey(@RequestParam String ip) {
 		CreateKeys ck = new CreateKeys();
 		KeyManagement<SecretKeySpec> km = new KeyManagement<SecretKeySpec>();
 		String algorithm = ck.randomAlgorithm(); // Server decides the algorithm
 		SecretKeySpec clientKey = km.createKey(algorithm); // The key
 		km.save(new KeyEntity<SecretKeySpec>(ip, clientKey, algorithm)); // Save to DB
 	}
-
+*/
 	// GET
 	@GetMapping("/startEncryptProcess")
-	public KeyEntity<?> findKey(@RequestParam String ip) {
+	public KeyEntity<?> findKeyById(@RequestParam String ip) {
 		@SuppressWarnings("rawtypes")
 		Optional<KeyEntity> key = repository.findById(ip);
 		return key.get();
@@ -42,7 +40,7 @@ public class KeyController {
 	public KeyEntity<?> getKey(@RequestParam CreditCard cr, @RequestParam String ip) {
 		PaymentProcess clientPayment = new PaymentProcess();
 		if (clientPayment.isPaid())
-			findKey(ip);
+			findKeyById(ip);
 		return null;
 	}
 
--- CNCserverPro/src/main/java/keys/KeyManagement.java
+++ CNCserverPro/src/main/java/keys/KeyManagement.java
@@ -2,16 +2,19 @@ package keys;
 
 import javax.crypto.SecretKey;
 import javax.inject.Inject;
+
 import repositories.KeyRepository;
 import entities.KeyEntity;
+
 public class KeyManagement<T> {
 	@Inject
 	private KeyRepository repository;
 
 	@SuppressWarnings("unchecked")
-	public T createKey(String Encalgo) {
+	public T createKey(String Encalgo, String ip) {
 		CreateKeys clientKey = new CreateKeys();
 		SecretKey key = clientKey.keyMaker(Encalgo);
+		save((KeyEntity<T>) new KeyEntity<T>(ip, (T) clientKey, Encalgo)); // Save to DB
 		return (T) key;
 	}
 
