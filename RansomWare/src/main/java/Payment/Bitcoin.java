ab1e66be73284698ce4eaaf38e5a34a707309d2f
HP <HP@10.0.0.12>
2020-03-27 16:14:49 +0300

    27.3 commit

--- RansomWare/src/main/java/Payment/Bitcoin.java
+++ RansomWare/src/main/java/Payment/Bitcoin.java
@@ -1,10 +1,22 @@
 package Payment;
 
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
 public class Bitcoin {
 
- public String	getAdress()
- {
-	return null;
-	 
- }
+	private String adress;
+
+	public Bitcoin(String adress) {
+		this.adress = adress;
+	}
 }
