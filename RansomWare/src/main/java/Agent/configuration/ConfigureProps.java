5ab99c28fa958a5564f00aece52e4409b63c1d63
osherlev <osherlev317@gmail.com>
2020-05-01 12:39:07 +0300

    check if properties file is initialized - 1.5 #2

--- RansomWare/src/main/java/Agent/configuration/ConfigureProps.java
+++ RansomWare/src/main/java/Agent/configuration/ConfigureProps.java
@@ -4,15 +4,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.hibernate.Hibernate;
+
 import Agent.exceptions.InOutException;
 
 public class ConfigureProps {
 	private static Properties properties;
 
 	public static String getPropsValue(String key) throws InOutException {
+
 		try (InputStream is = ConfigureProps.class.getResourceAsStream("/application.properties");) {
 			properties = new Properties();
-			properties.load(is);
+			if (!(Hibernate.isInitialized(properties))) {
+				properties.load(is);
+			}
 			return properties.getProperty(key);
 		} catch (IOException e) {
 			throw new InOutException("Could not find properties file", e);
