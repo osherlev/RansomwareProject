3ef408ae3b78c80344e99f08ecfa0ba0afb01661
osherlev <osherlev317@gmail.com>
2020-05-04 14:49:12 +0300

    Optional configuration

--- RansomWare/src/main/java/Agent/configuration/ConfigureProps.java
+++ RansomWare/src/main/java/Agent/configuration/ConfigureProps.java
@@ -2,24 +2,26 @@ package Agent.configuration;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
 import java.util.Properties;
 
 import Agent.exceptions.InOutException;
 
 public class ConfigureProps {
-	private static Properties properties = null;
+	private static Optional<Properties> properties;
 
 	public static String getPropsValue(String key) throws InOutException {
 		if (properties == null) {
 			try (InputStream is = ConfigureProps.class.getResourceAsStream("/application.properties");) {
 				{
-					properties = new Properties();
-					properties.load(is);
+					properties = Optional.ofNullable(new Properties());
+					properties.get().load(is);
+
 				}
 			} catch (IOException e) {
 				throw new InOutException("Could not find properties file", e);
 			}
 		}
-		return properties.getProperty(key);
+		return properties.get().getProperty(key);
 	}
 }
